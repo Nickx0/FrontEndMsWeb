25741219ff72779368b891ff30c1c394f7e2e65f
Tamiel <63595098+Tamiel@users.noreply.github.com>
2022-07-28 21:11:13 -0400

    Delete src/views/galeri directory
    código obsoleto

--- src/views/galeri/components/CuadroListItem.js
+++
@@ -1,8 +0,0 @@
-export default function CuadroListItem({value}) {
-    return (
-        <div className="galery-card">
-            <img className="card-img-top" src={`./img/${value.src}.jpg`} alt='Misora'/>
-            <a className="btn btn-danger " href={ value.autor } target="_blank" rel='noreferrer'>Autor</a>
-        </div>
-    )
-}
