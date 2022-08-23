b7f66481c76ed5e6464a3b71b1e30424f5deeede
Nick Ochoa Sicha <79413378+Nickx0@users.noreply.github.com>
2022-08-23 13:14:47 -0500

    More Public components

---
+++ src/views/components/Error.js
@@ -0,0 +1,11 @@
+const Error =()=>{
+    return(
+        <div className="FakeContent">
+            <div  className="fakeconstruction">
+            <img className="construction-image" src="./utilities/MisoraIdol.webp" alt="Hina-Misora"/>
+            <b className="construction">PAGINA EN CONSTRUCCION...</b>
+            </div>
+        </div>
+    )
+};
+export default Error;
---
+++ src/views/components/Void.js
@@ -0,0 +1,13 @@
+export default function Void(){
+    return(
+        <div className="FakeContent">
+            <div>
+            <div className="spinner">
+
+            </div>
+            <b>LOADING....</b>
+            </div>
+
+        </div>
+    )
+}
