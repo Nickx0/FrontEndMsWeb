0c778f152e8e2def2beffde409559eebae05a260
Nick Ochoa Sicha <79413378+Nickx0@users.noreply.github.com>
2022-08-23 13:16:13 -0500

    Add files via upload

--- src/views/404.js
+++ src/views/404.js
@@ -1,12 +1,11 @@
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import Error from './components/Error'
 export default function Forzerofor ( ){
     return(
       <>
       <Navbar />
-      <div>
-        NOT FOUND
-      </div>
+      <Error/>
       <Footer />
       </>
     )
