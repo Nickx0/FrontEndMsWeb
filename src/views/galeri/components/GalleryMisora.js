15fe5fba7db1c6c8359ab7f58bf67ec725e4c2d8
Nick Ochoa Sicha <79413378+Nickx0@users.noreply.github.com>
2022-08-23 13:15:14 -0500

    Add files via upload

--- src/views/galeri/components/GalleryMisora.js
+++ src/views/galeri/components/GalleryMisora.js
@@ -3,12 +3,15 @@ import Cuadro from './Cuadro';
 import ExpandedImg from './ExpandedImg';
 import GaleryZus from '../../../stores/galeryzus';
 import shallow from 'zustand/shallow';
+import Void from '../../components/Void'
 
 export default function GalleryMisora(){
-    const{ urls, setUrls,imgExpanded } = GaleryZus(({ urls, setUrls,imgExpanded }) =>({ urls, setUrls,imgExpanded }), shallow);
+    const{ loading,error,urls, setUrls,imgExpanded } = GaleryZus(({ loading,error,urls, setUrls,imgExpanded }) =>({ loading,error,urls, setUrls,imgExpanded }), shallow);
     useEffect(() => {
         setUrls().catch(null);
     }, []);
+    if(loading) return (<Void/>);
+    if(error) return (<div>OCURRIO UN ERROR, VUELVA A CARGAR O ESPERE</div>)
     return (
         <div className='galery-misora'>
             { urls?.map((cuadro) => (<Cuadro key={cuadro._id} value={cuadro}/>)) }
