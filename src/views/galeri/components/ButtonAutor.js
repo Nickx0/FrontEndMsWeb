1edc7ee9974aae25b222cd0a18baaae835b08fb7
Nick Ochoa Sicha <79413378+Nickx0@users.noreply.github.com>
2022-08-26 18:26:03 -0500

    Change Id

--- src/views/galeri/components/ButtonAutor.js
+++ src/views/galeri/components/ButtonAutor.js
@@ -1,7 +1,7 @@
 export default function ButtonAutor({ redSocial, autor }){
     let redirect = () => {if (redSocial === 'twitter') return `https://twitter.com/${autor}`};
     return (
-        <div className='btn-cuadro-misora'>
+        <div className='btn-cuadro-misora' id="galery-boton">
             <label className='autor' >Autor: </label>
             <a href={redirect()} target="_blank" rel='noreferrer'>
                 <img className='icono-red-social' src="./icons/twitter-svgrepo-com.svg" alt="twitter" />
